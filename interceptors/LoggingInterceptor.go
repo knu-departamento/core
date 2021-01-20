7991ca162b9fc2181f65c68def3529effb39a577
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-01-20 15:46:44 +0200

    Just testing

--- interceptors/LoggingInterceptor.go
+++ interceptors/LoggingInterceptor.go
@@ -2,9 +2,11 @@ package interceptors
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
 )
 
 func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	fmt.Println("a")
 	return handler(ctx, req)
 }
