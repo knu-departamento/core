db461e06c46a9c69ee2971e89f361f2f7013cfdb
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-01-28 15:32:33 +0200

    Added fields to RoleTokenClaims

--- jwt/jwt_claims.go
+++ jwt/jwt_claims.go
@@ -3,12 +3,15 @@ package jwt
 import "github.com/dgrijalva/jwt-go"
 
 type AccountTokenClaims struct {
-	Level string `json:"lvl"`
 	Type  string `json:"typ"`
 	Email string `json:"email"`
 	jwt.StandardClaims
 }
 
 type RoleTokenClaims struct {
+	Type         string `json:"typ"`
+	Email        string `json:"email"`
+	Role         string `json:"rol"`
+	DepartmentId int    `json:"dep_id"`
 	jwt.StandardClaims
 }
--- jwt/validation.go
+++ jwt/validation.go
@@ -53,7 +53,7 @@ func ValidateJwtAccountRefreshToken(tokenString string) (jwt.MapClaims, error) {
 		return nil, errors.New("invalid token")
 	}
 
-	if !(!claims.VerifyIssuer("departamento", true) || claims.VerifyIssuedAt(time.Now().Unix(), true) || claims.VerifyExpiresAt(time.Now().Unix(), true) || claims["lvl"] != "account" || claims["typ"] != "refresh") {
+	if !(!claims.VerifyIssuer("departamento", true) || claims.VerifyIssuedAt(time.Now().Unix(), true) || claims.VerifyExpiresAt(time.Now().Unix(), true) || claims["typ"] != "refresh") {
 		return nil, errors.New("invalid token")
 	}
 
